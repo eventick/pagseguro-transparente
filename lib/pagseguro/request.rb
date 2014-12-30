995ae354efb5a307fbda4b130d744376116f6b49
Breno Oliveira <breno26@gmail.com>
2014-12-30 11:08:37 -0200

    refactoring for URL

--- lib/pagseguro/request.rb
+++ lib/pagseguro/request.rb
@@ -5,24 +5,26 @@ module PagSeguro
     include HTTParty
     debug_output $stderr
 
-    base_uri "https://ws.pagseguro.uol.com.br/v2/"
-
-    def initialize
-      if PagSeguro.sandbox?
-        base_uri = "https://ws.sandbox.pagseguro.uol.com.br/v2/"
-      end
-    end
-
     def get(path, account = "default")
+      self.class.base_uri sandbox_uri!
       options = { query: add_credencials(account) }
       self.class.get(path, options)
     end
 
     def post(path, account = "default", params = {})
+      self.class.base_uri sandbox_uri!
       options = { body: add_credencials(account) }
       options[:body].merge!(params)
       options[:timeout] = PagSeguro.timeout unless PagSeguro.timeout.blank?
       self.class.post(path, options)
+    end
+
+    def sandbox_uri!
+      if PagSeguro.sandbox?
+        return "https://ws.sandbox.pagseguro.uol.com.br/v2/"
+      end
+
+      "https://ws.pagseguro.uol.com.br/v2/"
     end
 
     private
